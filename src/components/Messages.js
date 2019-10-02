154fecd1a672e53ef150a84ec4dadf711892f946
poushitaguha <pguha.kolkata@gmail.com>
2019-10-02 15:32:24 -0400

    Add scroll to bottom for messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -2,40 +2,56 @@ import React, { Component } from 'react';
 import './styles/messages.css'
 
 class Messages extends Component {
+
+  messagesEnd = React.createRef();
+
+  componentDidUpdate() {
+    this.scrollToBottom()
+  }
+
+  scrollToBottom = () => {
+    this.messagesEnd.current.scrollIntoView({ behavior: 'smooth' })
+  }
+
   render() {
     if (!this.props.msgs) {
       return (<div>No messages found!</div>)
     }
 
     const messages = this.props.msgs.map((msg, index) => {
-      if(msg.other_user_id === this.props.currentProfile){
-        if(msg.msg_dir === 'fromuser'){
-          return (<li className="message message-item-fromuser" key={index}>
-            <h5>Me:</h5>
-            <p>{msg.content}</p>
-            <p className="date-text">{new Date(msg.created_at).toLocaleString(
-              'en-US', 
-              { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
-            )}</p>
-          </li>)
+      if (msg.other_user_id === this.props.currentProfile) {
+        if (msg.msg_dir === 'fromuser') {
+          return (
+            <li className="message message-item-fromuser" key={index}>
+              <h5>Me:</h5>
+              <p>{msg.content}</p>
+              <p className="date-text">{new Date(msg.created_at).toLocaleString(
+                'en-US',
+                { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
+              )}</p>
+            </li>
+          )
         }
         return (
           <li className="message message-item-touser" key={index}>
             <h5>{msg.first_name} {msg.last_name}:</h5>
             <p>{msg.content}</p>
             <p className="date-text">{new Date(msg.created_at).toLocaleString(
-              'en-US', 
+              'en-US',
               { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' }
-            )}</p> 
+            )}</p>
           </li>
         )
       }
-      return (<div key={index}></div>)
+      return (
+        <div key={index}></div>
+      )
     })
 
     return (
       <ul className="message-list">
         {messages}
+        <div ref={this.messagesEnd}></div>
       </ul>
     )
   }
